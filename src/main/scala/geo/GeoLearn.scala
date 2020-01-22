c0b78296d4581711483456cb36df4f7703d43f9e
Rafa <rafaeldelacueva@gmail.com>
2020-01-22 13:15:27 +0100

    third upload

--- src/main/scala/geo/GeoLearn.scala
+++ src/main/scala/geo/GeoLearn.scala
@@ -2,18 +2,23 @@ package geo
 
 import java.io.File
 
+import com.vividsolutions.jts.geom.Geometry
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.serializer.KryoSerializer
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.analysis.FunctionRegistry
+import org.apache.spark.sql.geosparksql.expressions.ST_Point
+import org.apache.spark.sql.types.DataType
 import org.apache.spark.{SparkConf, SparkContext}
 import org.datasyslab.geospark.enums.FileDataSplitter
 import org.datasyslab.geospark.formatMapper.shapefileParser.ShapefileReader
 import org.datasyslab.geospark.formatMapper.{GeoJsonReader, WktReader}
 import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator
-import org.datasyslab.geospark.spatialRDD.{PointRDD, PolygonRDD}
+import org.datasyslab.geospark.spatialRDD.{PointRDD, PolygonRDD, SpatialRDD}
+import org.datasyslab.geosparksql.utils.Adapter
 
 import scala.collection.JavaConverters._
 import scala.io.Source
-
 
 object GeoLearn {
 
@@ -30,6 +35,11 @@ object GeoLearn {
     conf.set("spark.kryo.registrator", classOf[GeoSparkKryoRegistrator].getName)
 
     val sc = new SparkContext(conf)
+    val sparkSession = SparkSession
+      .builder()
+      .appName("SparkSessionZipsExample")
+//      .enableHiveSupport()
+      .getOrCreate()
 
     val pointRDDInputLocation = "./resources/checkin.csv"
     val pointRDDOffset = 0
@@ -52,21 +62,19 @@ object GeoLearn {
 //    println(s"polygonRDD count ${polygonRDD.countWithoutDuplicates()}")
 //    println(s"polygonRDD\n ${polygonRDD.getRawSpatialRDD.collect().asScala.mkString("\n")}")
 
-
     val wktInputLocation = "./resources/WKT.csv"
     val wktColumn = 0
     val allowTopologyInvalidGeometries = true
     val skipSyntaxInvalidGeometries = false
 
-
 //    println("WKT file\n" + Source.fromFile(wktInputLocation).getLines().mkString("\n"))
-//    val wktRDD = WktReader.readToGeometryRDD(sc, wktInputLocation, wktColumn, allowTopologyInvalidGeometries, skipSyntaxInvalidGeometries)
+    val wktRDD = WktReader.readToGeometryRDD(sc, wktInputLocation, wktColumn, allowTopologyInvalidGeometries, skipSyntaxInvalidGeometries)
 //
 //    println(s"wktRDD count ${wktRDD.countWithoutDuplicates()}")
 //    println(s"wktRDD\n ${wktRDD.getRawSpatialRDD.collect().asScala.mkString("\n")}")
 
-//    val geoJsonInputLocation = "./resources/polygon.json"
-//    val geoJsonlRDD = GeoJsonReader.readToGeometryRDD(sc, geoJsonInputLocation, allowTopologyInvalidGeometries, skipSyntaxInvalidGeometries)
+    val geoJsonInputLocation = "./resources/polygon.json"
+    val geoJsonlRDD = GeoJsonReader.readToGeometryRDD(sc, geoJsonInputLocation, allowTopologyInvalidGeometries, skipSyntaxInvalidGeometries)
 //
 //    println(s"geoJsonlRDD count ${geoJsonlRDD.countWithoutDuplicates()}")
 //    println(s"geoJsonlRDD\n ${geoJsonlRDD.getRawSpatialRDD.collect().asScala.mkString("\n")}")
@@ -74,8 +82,52 @@ object GeoLearn {
     val shapefileInputLocation="./resources/shapefile"
     val shapefileRDD = ShapefileReader.readToGeometryRDD(sc, shapefileInputLocation)
 
-    println(s"shapefileRDD count ${shapefileRDD.countWithoutDuplicates()}")
-    println(s"shapefileRDD\n ${shapefileRDD.getRawSpatialRDD.collect().asScala.mkString("\n")}")
+//    println(s"shapefileRDD count ${shapefileRDD.countWithoutDuplicates()}")
+//    println(s"shapefileRDD\n ${shapefileRDD.getRawSpatialRDD.collect().asScala.mkString("\n")}")
+
+    val sqlDf = sparkSession.read.format("csv").option("delimiter", ",").option("header", "false").load(pointRDDInputLocation)
+    sqlDf.createOrReplaceTempView("inputtable")
+
+    FunctionRegistry.builtin.registerFunction("ST_Point", ST_Point)
+    sparkSession.udf.register("ST_Point", ST_Point)
+
+    println(s"sqlDf\n")
+    sqlDf.show()
+
+    println(s"schema\n")
+    sqlDf.printSchema()
+
+    val castDf = sqlDf
+      .withColumn("_c0",sqlDf.col("_c0").cast("double"))
+      .withColumn("_c1",sqlDf.col("_c1").cast("double"))
+    sqlDf.createOrReplaceTempView("castinputtable")
+
+    println(s"castDf\n")
+    castDf.show()
+
+    println(s"castDfschema\n")
+    castDf.printSchema()
+
+//    val spatialSqlDf = sparkSession.sql(
+//      """
+//        |SELECT ST_Point(CAST(castinputtable._c0 AS Decimal(24,20)),CAST(castinputtable._c1 AS Decimal(24,20))) AS checkin
+//        |FROM castinputtable
+//    """.stripMargin)
+//
+////    val spatialSqlDf = sparkSession.sql(
+////      """
+////        |SELECT ST_Point(castinputtable._c0,castinputtable._c1) AS checkin
+////        |FROM castinputtable
+////    """.stripMargin)
+//
+//    val sqlSpatialRDD = new SpatialRDD[Geometry]
+//    sqlSpatialRDD.rawSpatialRDD = Adapter.toRdd(spatialSqlDf)
+//
+//    println(s"spatialSqlDf\n")
+//    spatialSqlDf.show()
+
+//    println(s"shapefileRDD\n ${spatialSqlDf.rawSpatialRDD.collect().asScala.mkString("\n")}")
+
 
   }
 }
